cfc8b9604f76c029170a5bb5e3cfd613192f4739
moritzebeling <git@moritzebeling.com>
2023-06-03 20:51:32 +0200

    complete new version

--- index.php
+++ index.php
@@ -1,115 +1,209 @@
 <?php
 
-Kirby::plugin('moritzebeling/meta', [
+use Kirby\Cms\App as Kirby;
+use Kirby\Cms\Response;
+use Kirby\Cms\Html;
+use Kirby\Toolkit\Str;
 
-	'options' => [
-		'fieldnames' => [
-			'description' => 'description',
-			'tags' => 'tags',
-			'image' => false,
-		],
-		'thumbs' => [
-			'presets' => [
-				'ogimage' => [ 'width' => 2000, 'height' => 2000, 'crop' => true ]
-			]
-		]
-	],
+function jsonld( array $data ){
+    return Html::tag(
+        'script',
+        json_encode($data, JSON_NUMERIC_CHECK | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES ),
+        ['type' => 'application/ld+json']
+    );
+}
 
-	'siteMethods' => [
-		'jsonLd' => function (): array {
+Kirby::plugin('moritzebeling/kirby-meta', [
 
-			$json = [
-				'@context' => 'https://schema.org/',
-				'@type' => 'WebSite',
-				'copyrightYear' => date('Y'),
-				'name' => $this->title()->value(),
-				'url' => $this->url()
-			];
+    'options' => [
+        'title_schema' => '{page} | {site}',
+        'preview_image.resize' => [
+            'width' => 1200,
+            'height' => 630,
+            'crop' => true,
+        ],
+        // page-id
+        // /^page-id$/
+        // /parent\/.*/
+        'ignore' => ['error','panel']
+    ],
 
-			if( $this->kirby()->languages()->count() > 1 ){
-				$json['inLanguage'] = [];
-				foreach( $this->kirby()->languages() as $lang ){
-					$jsonld['inLanguage'] = [
-						'@type' => 'Language',
-						'name' => $lang->name(),
-					];
-				}
-			}
+    'blueprints' => [
+        'fields/meta/meta' => __DIR__ . '/blueprints/field-meta.yml',
+        'tabs/meta/page' => __DIR__ . '/blueprints/tab-page.yml',
+        'tabs/meta/site' => __DIR__ . '/blueprints/tab-site.yml',
+    ],
 
-			return $json;
-		},
-	],
+    'snippets' => [
+        'meta' => __DIR__ . '/snippets/meta.php',
+        'meta/sitemap' => __DIR__ . '/snippets/sitemap.php',
+        'meta/robots' => __DIR__ . '/snippets/robots.php',
+    ],
 
-	'pageMethods' => [
-		'metaDescription' => function (): string {
+    'siteMethods' => [
+        'meta' => function () {
+            $site_meta = $this->content()->meta()->toObject();
+            if( $site_meta->title()->isEmpty() ){
+                $site_meta->update([
+                    'title' => (string)$this->title(),
+                ]);
+            }
+            return $site_meta;
+        },
+        'schema' => function (): array {
 
-			$fieldname = option('moritzebeling.meta.fieldnames.description');
+            $meta = $this->meta();
 
-			if( $this->{$fieldname}()->isNotEmpty() ){
-				return $this->{$fieldname}()->value();
-			}
-			if( $this->site()->{$fieldname}()->isNotEmpty() ){
-				return $this->site()->{$fieldname}()->value();
-			}
-			return $this->site()->title()->value();
+            $thumbnail = asset( option('moritzebeling.kirby-favicon.favicon.png') );
 
-		},
-		'metaKeywords' => function (): array {
+            if( $image = $meta->image()->toFile() ){
+                $image = $image->thumb(option('moritzebeling.kirby-meta.preview_image.resize'))->url();
+            }
 
-			$fieldname = option('moritzebeling.meta.fieldnames.tags');
+            return [
+                '@context' => 'https://schema.org',
+                '@type' => 'WebSite',
+                '@id' => $this->url(),
+                'alternateName' => (string)$this->title(),
+                'copyrightYear' => date('Y'),
+                'description' => (string)$meta->description()->escape(),
+                'image' => $image,
+                'inLanguage' => $this->kirby()->languages()->pluck('code'),
+                'keywords' => $meta->keywords()->split(),
+                'name' => (string)$meta->title(),
+                'sameAs' => array_map(function($field){
+                    return (string)$field;
+                },$this->links()->toStructure()->pluck('url')),
+                'thumbnailUrl' => $thumbnail->exists() ? $thumbnail->url() : null,
+                'url' => $this->url(),
+            ];
+        },
+    ],
+    'pageMethods' => [
+        'meta' => function () {
 
-			$tags = array_unique( array_merge(
-				$this->{$fieldname}()->split(),
-				$this->site()->{$fieldname}()->split()
-			));
-			return array_slice( $tags, 0, 12 );
+            $page_meta = $this->content()->meta()->toObject();
+            $site_meta = $this->site()->meta();
+                        
+            $title = $this->site()->meta_title_schema()->or( option('moritzebeling.kirby-meta.title_schema','{page} | {site}') );
+            $title = str_replace( '{site}', $site_meta->title(), $title );
+            $title = str_replace( '{page}', $page_meta->title()->or( $this->title() ), $title );
 
-		},
-		'ogImage' => function ( ?string $fieldname = null ) {
+            $description = $page_meta->description()->or( $site_meta->description() );
 
-			$fieldname = $fieldname ? $fieldname : option('moritzebeling.meta.fieldnames.image');
+            $keywords = array_slice(array_unique( array_merge(
+                $page_meta->keywords()->split(),
+                $site_meta->keywords()->split()
+            )),0,20);
 
-			if( $this->isHomePage() ){
-				if( $image = $this->site()->ogimage()->toFile() ){
-					return $image;
-				}
-			}
+            $image = $page_meta->image()->or( $site_meta->image() )->value();
 
-			if( $this->hasImages() ){
-				if( $image = $this->content()->titleImage()->toFile() ){
-					return $image;
-				} else {
-					return $this->image();
-				}
-			}
+            $page_meta->update([
+                'title' => $title,
+                'description' => (string)$description,
+                'keywords' => implode(',',$keywords),
+                'image' => $image,
+            ]);
 
-			return $this->site()->image();
+            return $page_meta;
+        },
+        'schema' => function (): array {
+            $meta = $this->meta();
 
-		},
-	],
+            $blocks = $this->blocks()->toBlocks();
+            
+            if( $about = $blocks->filterBy('type','text')->first() ){
+                $about = Str::unhtml((string)$about->text()->short(1000));
+            } else {
+                $about = null;
+            }
+            
+            if( $primaryImageOfPage = $blocks->filterBy('type','image')->first() ){
+                $primaryImageOfPage = $primaryImageOfPage->image()->toFile()->thumb(['width' => 1200])->url();
+            }
 
-	'fileMethods' => [
-		'alt' => function (): Kirby\Cms\Field {
+            if( $image = $meta->image()->toFile() ){
+                $image = $image->thumb(option('moritzebeling.kirby-meta.preview_image.resize'))->url();
+            }
 
-			if( $this->content()->title()->isNotEmpty() ){
-				return $this->content()->title();
-			}
-			return $this->parent()->title();
+            return [
+                '@context' => 'https://schema.org',
+                '@type' => 'WebPage',
+                '@id' => $this->site()->url() . '/' . $this->id(),
+                'about' => $about,
+                'alternateName' => (string)$this->title(),
+                'copyrightYear' => date('Y'),
+                'dateModified' => $this->modified('c', 'date'),
+                'description' => (string)$meta->description()->escape(),
+                'image' => $image,
+                'inLanguage' => $this->kirby()->languages()->pluck('code'),
+                'keywords' => $meta->keywords()->split(),
+                'mainEntityOfPage' => [
+                    '@type' => 'WebSite',
+                    '@id' => $this->site()->url(),
+                ],
+                'name' => (string)$meta->title(),
+                'primaryImageOfPage' => $primaryImageOfPage,
+                'url' => $this->url(),
+            ];
+        }
+    ],
 
-		},
-		'title' => function (): Kirby\Cms\Field {
+    'routes' => [
+        [
+            'pattern' => 'sitemap.xml',
+            'action'  => function() {
 
-			if( $this->content()->title()->isNotEmpty() ){
-				return $this->content()->title();
-			}
-			return new Field( $this, 'title', $this->filename() );
+                $kirby = kirby();
+                $ignore = $kirby->option('moritzebeling.kirby-meta.ignore');
 
-		},
-		'caption' => function (): Kirby\Cms\Field {
+                $pages = $kirby->site()->index()->listed()->filter(function($page) use($ignore){
+                    if( $page->isHomePage() ){
+                        return true;
+                    }
+                    if( $page->noindex()->isTrue() ){
+                        return false;
+                    }
+                    foreach( $ignore as $pattern ){
+                        if( preg_match("/^\/.+\/[a-z]*$/i",$pattern) ){
+                            // regex
+                            if( preg_match($pattern, $page->id()) ){
+                                return false;
+                            }
+                        } else {
+                            // page id
+                            if( $pattern == $page->id() ){
+                                return false;
+                            }
+                        }
+                    }
+                    return true;
+                })->limit(50000);
 
-			return $this->content()->caption();
-
-		}
-	]
+                $content = snippet('meta/sitemap', compact('pages'), true);
+                return new Response($content, 'application/xml');
+            }
+        ],
+        [
+            'pattern' => 'sitemap',
+            'action'  => function() {
+                return go('sitemap.xml', 301);
+            }
+        ],
+        [
+            'pattern' => 'robots.txt',
+            'action'  => function() {
+                $rules = [[
+                    'User-agent' => '*',
+                    'Allow' => '/',
+                    'Disallow' => '/panel'
+                ],[
+                    'Sitemap' => url('sitemap.xml')
+                ]];
+                $content = snippet('meta/robots', compact('rules'), true);
+                return new Response($content, 'text/plain');
+            }
+        ]
+    ],
 
 ]);
---
+++ snippets/meta.php
@@ -0,0 +1,41 @@
+<?php
+
+$site_meta = $site->meta();
+$page_meta = $page->meta();
+
+?>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<title><?= $page_meta->title() ?></title>
+
+<?php if( option('debug', false) || $page_meta->noindex()->isTrue() ): ?>
+    <meta name="robots" content="noindex">
+<?php endif ?>
+
+<link rel="canonical" href="<?= $page->url() ?>" />
+<meta name="description" content="<?= $page_meta->description() ?>">
+<meta name="keywords" content="<?= $page_meta->keywords() ?>">
+
+<meta property="og:title" content="<?= $page_meta->title() ?>">
+<meta property="og:description" content="<?= $page_meta->description() ?>">
+<meta property="og:url" content="<?= $page->url() ?>">
+<meta property="og:type" content="website">
+<meta property="og:site_name" content="<?= $site_meta->title() ?>">
+<meta property="og:locale" content="<?= (string)$kirby->language()->locale()[0] ?>">
+
+<?php if( $image = $page_meta->image()->toFile() ):
+    $image = $image->thumb(option('moritzebeling.kirby-meta.preview_image.resize'))->url();
+    ?>
+    <meta property="og:image" content="<?= $image ?>">
+    <meta name="twitter:image" content="<?= $image ?>">
+<?php endif ?>
+
+<meta name="format-detection" content="telephone=no">
+<meta name="Generator" content="Moritz Ebeling (https://moritzebeling.com)">
+
+<?php foreach ($kirby->languages()->not( $kirby->language() ) as $language): ?>
+    <link rel="alternate" hreflang="<?= $language->code() ?>" href="<?= $page->url($language->code()) ?>" />
+<?php endforeach ?>
+
+<?= jsonld($site->schema()) ?>
+<?= jsonld($page->schema()) ?>
